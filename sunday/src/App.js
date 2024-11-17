4a24a9c4f73dd16797cbdd751a092bc2ac5400a1
Mehdi <mehdiabdelaoui0@gmail.com>
2024-11-17 17:56:38 +0100

    mehdi

---
+++ sunday/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+
+
+function App() {
+  return (
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#191b1f",
+          height: "100vh",
+          direction: "rtl",
+        }}
+      >
+        <TodoList />
+      </div>
+   
+  );
+}
+
+export default App;
---
+++ sunday/src/components/TodoList.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+export default function TodoList() {
+  return (
+    <>
+      <CardContent>  
+    </CardContent>
+    <CardActions>
+     </CardActions>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </>
+  );
+}
